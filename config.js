3f654286fa98a19d70b899d29f9de984830e51f9
Evan Tam <evantam@Evans-MacBook-Pro-3.local>
2018-03-11 15:04:35 -0700

    removed port from google callback url

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || '';
 const SSL = process.env.SSL || 'http';
 const HOSTNAME = process.env.HOSTNAME || 'localhost';
 const PORT = process.env.PORT || 8080;
-const GOOGLE_CALLBACK_URL = `${SSL}://${HOSTNAME}:${PORT}/auth/google/callback`;
+// const GOOGLE_CALLBACK_URL = `${SSL}://${HOSTNAME}:${PORT}/auth/google/callback`;
+const GOOGLE_CALLBACK_URL = `${SSL}://${HOSTNAME}/auth/google/callback`;
 const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost:${PORT}/test`;
 
 const config = {
